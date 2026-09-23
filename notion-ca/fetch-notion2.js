b1c2803874792c426de8ace30292ede5bdca6f7f
kksk09 <kksk09@gmail.com>
2026-09-23 15:41:46 +0700

    Remove hardcoded Notion tokens

--- notion-ca/fetch-notion2.js
+++ notion-ca/fetch-notion2.js
@@ -1,8 +1,7 @@
 const { Client } = require("@notionhq/client");
 const fs = require("fs");
 
-// ================== ตั้งค่า ==================
-// const NOTION_TOKEN = "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";
+// ================== ตั้งค่า ================== 
 const NOTION_TOKEN = process.env.NOTION_TOKEN  ;
 const DATABASE_ID = "086e547f7b06430b84dbd082ba3824ea";
 const YEAR = process.argv[2] ? parseInt(process.argv[2]) : 2026;
